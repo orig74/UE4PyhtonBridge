22fdd1445563f12d51ed0bb6f83fd71da5692293
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2017-01-25 16:33:45 +1300

    major changes for supporting drone lab

--- src/Wrappers/__init__.py
+++ src/Wrappers/__init__.py
@@ -2,10 +2,8 @@
 import os
 import ctypes
 import glob,json
-if 'PATH_FILE' in os.environ:
-    path_file=os.environ['PATH_FILE']
-    path_data=json.load(open(path_file))
-    libfile=glob.glob(path_data['project_path']+'/Binaries/Linux/libUE4Editor*.so')[0]
+if 'UE4EDITOR_SO' in os.environ:
+    libfile=os.environ['UE4EDITOR_SO']
     print('loading libfile',libfile)
 else:
     libfile=''
@@ -13,5 +11,3 @@ else:
 print('start loading lib file')
 libc=ctypes.CDLL(libfile,mode=ctypes.RTLD_GLOBAL)
 print('done loading lib file')
-#libc.calledfrompython.argtypes=[]
-#libc.calledfrompython.restype=ctypes.c_int
